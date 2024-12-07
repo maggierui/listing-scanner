b2e5019003ef0612ca33006336be6168992d85e2
Rui Hu <maggiehu.china@gmail.com>
2024-12-07 18:14:49 -0500

    add static file hdandling

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ async function trackApiCall() {
     }
 }
 
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+app.use(express.static('public'));
+
 
 
 
